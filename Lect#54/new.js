9605227187340f00f6f80c1db575364ae2caa7c3
NasirMehdi <nasirmehdi0101@gmail.com>
2025-03-19 01:31:37 +0500

    First Lecture of javascipt

---
+++ Lect#54/new.js
@@ -0,0 +1,4 @@
+console.log("^^^^^HELLO WORLD^^^^")
+console.log("Code is running")
+console.log("Code is also running")
+document.title = " Hey Im a good "
---
+++ Lect#54/script.js
@@ -0,0 +1,12 @@
+alert("Hello my first Lecture of javascript")
+console.log("Code is running")
+console.log("Code is also running")
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you student") 
+if(isTrue){
+    console.log("Yes student")
+}
+else{
+    console.log("Im not a student")
+}
+console.log("Yor number is:"  + a)
