1c68cc05de426056a088612e470d20b364e60b13
NasirMehdi <nasirmehdi0101@gmail.com>
2025-04-28 18:16:34 +0500

    Loops

---
+++ Lect#57/script.js
@@ -0,0 +1,44 @@
+// console.log("Hey! I am a tutorial on loops")
+// let a = 1;
+// console.log(a+1)
+// console.log(a+2)
+// console.log(a+3)
+// // for loop
+// for (let i = 0; i < 10; i++) {
+//     console.log(a+i);
+// }
+//  let obj = {
+//     name: "Mehdi",
+//     role: "Software engineer",
+//     company: "Dynamic_Leo"
+//  }
+
+// for in loop
+//  for (const key in obj) 
+// {
+//       const element = obj[key];
+//        console.log(key,element)            
+//  }
+
+// For off loop
+
+// for (const char of Nasir) 
+//     {
+//     console.log(char)
+// }
+
+
+//  while loop
+// let n = 0;
+// while(n<6){
+//     console.log(n)
+//     n++;
+// }
+
+// do while loop
+
+let n = 10;
+do{
+    console.log(n)
+    n++;
+} while(n<6);
